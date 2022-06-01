64f13c1565072444b2f580871ca28a24ab6e48f3
Ariel <cestari.ariel@gmail.com>
2022-06-01 11:49:05 +0100

    Creacion del proyecto Spring

---
+++ primer-microservicio/src/test/java/com/ariel/microservicios/primero/primermicroservicio/PrimerMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ariel.microservicios.primero.primermicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
